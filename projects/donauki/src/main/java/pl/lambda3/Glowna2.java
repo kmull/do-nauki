be2cd365b879122ce05bb02d88070339c9a8a871
Krzysztof Muller <krzysztofmuller1@gmail.com>
2017-11-03 17:41:03 +0100

    new

---
+++ projects/donauki/src/main/java/pl/lambda3/Glowna2.java
@@ -0,0 +1,15 @@
+package pl.lambda3;
+
+public class Glowna2 {
+    String name;
+
+    public Glowna2(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
